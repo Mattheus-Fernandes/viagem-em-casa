09049428c852ce023fa1ac836a0b75753385d7e7
Mattheus Fernandes <mattheus.ale@hotmail.com>
2022-10-15 22:36:37 -0300

    remocao de rotas

--- start/startController.js
+++ start/startController.js
@@ -18,25 +18,7 @@ route.get("/continentes", (req, res) => {
     res.render("start/continente")
 })
 
-route.get("/continente/lugares", (req, res) => {
-    
-    const continente = req.query.continente
-    console.log(continente)
-    
-    if( continente === "América"){
-        res.redirect("/continente/lugares/america")
-    } else if(continente === "Europa") {
-        res.redirect("/continente/lugares/europa")
-    } else if(continente === "África"){
-        res.redirect("/continente/lugares/africa")
-    }else if(continente === "Ásia"){
-        res.redirect("/continente/lugares/asia")
-    }else if(continente === "Oceania"){
-        res.redirect("/continente/lugares/oceania")
-    }else if(continente === "Antártida"){
-        res.redirect("/continente/lugares/antartida")
-    }
-})
+
 
 module.exports = route
 
